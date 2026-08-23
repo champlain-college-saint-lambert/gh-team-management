89825ab2b67541cdcc19e302cfb1ecc161cad5ea
nic5694 <nicholasmartoccia04@icloud.com>
2026-08-23 18:16:34 -0400

    fix rbac

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,21 +9,10 @@ resource "github_team" "workshop" {
   create_default_maintainer = true
 }
 
-resource "github_membership" "org_members" {
-  for_each = toset(local.members_data["git_workshop"])
-
-  username = each.value
-  role     = "member"
-}
-
-# 2. Add them to the team as a 'maintainer'
 resource "github_team_membership" "workshop_members" {
   for_each = toset(local.members_data["git_workshop"])
 
   team_id  = github_team.workshop.id
   username = each.value
-  role     = "maintainer"
-
-  # Ensure they are invited to the org before being added to the team
-  depends_on = [github_membership.org_members]
+  role     = "member"
 }
